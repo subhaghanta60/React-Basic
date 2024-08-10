6825fb679b239c343b1bf73b01814e385e12ed69
Subha Ghanta <Subhaghanta60@gmail.com>
2024-08-10 12:11:40 +0530

    Redux Toolkit

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './compnonents/AddTodo'
+import Todos from './compnonents/Todos'
+
+function App() {
+
+  return (
+    <>
+    <h1>Learn About Redus Toolkit</h1>
+     <AddTodo />
+     <Todos />
+    </>
+  )
+}
+
+export default App
