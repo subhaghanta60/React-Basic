0dac8a5b3b1bd7e370af17eaa416a3cd483f1d2e
Subha Ghanta <Subhaghanta60@gmail.com>
2024-08-12 23:14:26 +0530

    Product Level ENV File Set UP

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+import conf from './config/config';
+import './App.css'
+
+function App() {
+  
+console.log(conf.appwriteUrl);
+  return (
+    <>
+     <h1>A Mega Blog App With AppWrite</h1>
+    </>
+  )
+}
+
+export default App
