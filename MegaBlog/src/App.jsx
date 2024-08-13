3a7577c2ffaf16b172f193496feacc2ed6153433
Subha Ghanta <Subhaghanta60@gmail.com>
2024-08-14 00:27:22 +0530

    Set Up Redux Toolkit

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,14 +1,43 @@
 
+import { useState,useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
 import './App.css'
+import { Footer, Header } from './components'
+import {login,logout} from "./store/authSlice"
 
 function App() {
+  const [loading,setLoading]=useState(true);
+  const dispatch = useDispatch();
+
+
+  useEffect(()=> {
+     authService.getCurrentUSer()
+     .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+      
+     })
+     .finally(()=> setLoading(false))
+  },[])
   
 
-  return (
-    <>
-     <h1>A Mega Blog App With AppWrite</h1>
-    </>
-  )
+
+ return !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header />
+            <main>
+           
+            </main>
+         
+          <Footer />
+        </div>
+      </div>
+) : null
 }
 
 export default App
