85d76617f6edfb0d144af4a8b6f75089a6c5017c
Subha Ghanta <Subhaghanta60@gmail.com>
2024-08-15 11:09:30 +0530

    Building All Pages

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,10 +3,14 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Signup from "./Signup"
+import Login from "./Login"
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Signup,
+    Login
 }
